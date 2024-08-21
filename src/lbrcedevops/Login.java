d1c43b38fca433c037eae6d596e247322b52fe13
AdmiN <AdmiN@ITWS0040>
2024-08-21 15:33:17 +0530

    Commit

---
+++ src/lbrcedevops/Login.java
@@ -0,0 +1,45 @@
+package lbrcedevops;
+import java.util.Scanner;
+
+
+	public class Login {
+
+
+
+		public static void main(String[] args) {
+
+			// TODO Auto-generated method stub
+
+	        String uname="tejaprakashmarrapu@gmail.com";
+
+	        String pwd="hello";
+
+	        System.out.print("Welcome to Github");
+
+	        Scanner sc = new Scanner(System.in);
+
+	        System.out.print("Enter username");
+
+	        String uname1=sc.next();
+
+	        System.out.print("Enter password");
+
+	        String pwd1=sc.next();
+
+	        if(uname.equals(uname1)&&pwd.equals(pwd1)) {
+
+	        	System.out.print("Login Successfull");
+
+	        }
+
+	        else {
+
+	        	System.out.print("Login Failed");
+
+	        }
+
+		}
+
+
+
+	}
